59acfc44c61fffcbb7ee3f2c03c26cd6e8e8d05e
Ernesto Contreras <3rnestocs@gmail.com>
2024-05-03 22:50:43 -0400

    Correccion de archivos y codigo innecesario eliminado del package.json y otras carpetas.

--- src/api/modules/user.js
+++
@@ -1,7 +0,0 @@
-import { apiHttp } from "../axiosAPI";
-
-export const loginAPI = (loginValues) => apiHttp("POST", `/auth/login/`, loginValues)
-
-// export const whoAmIAPI = () => apiHttp("GET", `/v1/client/user/whoami`)
-
-export const registerAPI = (registerValues) => apiHttp("POST", `/auth/register/`, registerValues)
